8c93de02f51380f5d699fb760e02908d6a2eae93
UyenLai <uyenlai6@gmail.com>
2023-01-16 20:45:34 +0200

    multiply calculation - done

--- multiply-calculation/main.js
+++ multiply-calculation/main.js
@@ -2,12 +2,32 @@ const num1 = Math.floor((Math.random() * 10) + 1);
 const num2 = Math.floor((Math.random() * 10) + 1);
 let correctAns = num1 * num2;
 
-const question = document.getElementById('question');
-question.innerText = `What is ${num1} multiply by ${num2}?`
-const btn = document.querySelector('.btn');
-const userAns = document.getElementById('input');
-console.log(userAns);
+const questionEl = document.getElementById('question');
+questionEl.innerText = `What is ${num1} multiply by ${num2}?`
+const formEl = document.getElementById('form');
+const inputEl = document.getElementById('input');
+const scoreEl = document.getElementById('score');
+let score = JSON.parse(localStorage.getItem('score')); // localStorage.getItem('score') type is string
+                                                       // so have to convert it to a number
 
-btn.addEventListener('click', () => {
+/*in order to prevent there is no score in the localStorage then we have another if statement*/
+if (!score) {
+    score = 0;
+}
 
+scoreEl.innerText = `Score: ${score}`;
+
+formEl.addEventListener('submit', () => {
+   const userAns = +inputEl.value; // `+` converts string value to number
+   if (userAns === correctAns) {
+    score++;
+    updateScore();
+   }else {
+    score--;
+    updateScore();
+   }
 })
+
+function updateScore() {
+    localStorage.setItem('score', JSON.stringify(score));
+}
